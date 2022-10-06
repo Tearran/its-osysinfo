cade5b05d47a294cc65703a0c8a0ffa132a8932f
Joseph Turner <Tearran@users.noreply.github.com>
2022-10-06 12:05:50 -0700

    Update build.bash

--- build.bash
+++ build.bash
@@ -24,8 +24,34 @@ cd ..
 ## set the interprater
 echo '#!/usr/bin/env python3' | cat - $execName.zip > $installedName
 #
+#!/bin/bash
+
+
+#File location and name
+#SRC="${BASH_SOURCE[0]}"
+src="$(dirname ${BASH_SOURCE[0]})"
+cd $src || ( echo "faild build" && exit )
+echo $src
+buildName="${PWD##*/}"
+echo $buildName
+execName="${PWD##*/}.bin"
+
+echo $execName
+installedName=$( echo ${buildName} | sed 's|_|-|g' )
+echo $installedName
+
+## compress the contents to a zip
+#[ ! -e /usr/bin/zip ] && sudo apt update && sudo apt install zip
+#
+[ -e /usr/bin/zip ] && zip -r ../$execName.zip ./* 
+#
+cd ..
+#
+## set the interprater
+echo '#!/usr/bin/env python3' | cat - $execName.zip > $installedName
+#
 ## install the exicutible
-# sudo pip3 install luma.core luma.lcd luma.oled
+
 sudo install -b -v $installedName /usr/bin/
 sudo install "$src/its-osysinfo.service" "/etc/systemd/system/its-osysinfo.service"
 sudo sudo systemctl enable its-osysinfo.service
