83303b4e9e4282f8d4efb952bcf32409f5945c5e
Joseph Turner <Tearran@users.noreply.github.com>
2022-10-07 21:30:14 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,29 +4,30 @@
 #File location and name
 #SRC="${BASH_SOURCE[0]}"
 src="$(dirname ${BASH_SOURCE[0]})"
-cd $src || ( echo "faild build" && exit )
-echo $src
 buildName="${PWD##*/}"
-echo $buildName
 execName="${PWD##*/}.bin"
-
-echo $execName
 installedName=$( echo ${buildName} | sed 's|_|-|g' )
-echo $installedName
+
+cd $src || ( echo "faild build" && exit )
+
+yes | rm -r .git || ( echo "faild rm .git" && exit )
 
 ## compress the contents to a zip
-[ ! -e /usr/bin/zip ] && sudo apt update && sudo apt install zip
-#
+sudo apt install zip git python3-pip python3-pil libopenjp2-7
+sudo pip3 install luma.core luma.lcd luma.oled psutil
+
 [ -e /usr/bin/zip ] && zip -r ../$execName.zip ./* 
-#
+
 cd ..
-#
+
 ## set the interprater
 echo '#!/usr/bin/env python3' | cat - $execName.zip > $installedName
-#
-## install the exicutible
 
+## install the exicutible
 sudo install -b -v $installedName /usr/bin/
-sudo install "$src/its-osysinfo.service" "/etc/systemd/system/its_osysinfo.service"
-sudo sudo systemctl enable its-osysinfo.service
-sudo sudo systemctl start its-osysinfo.service
+
+cd "$HOME/.local/src/its_osysinfo/"
+
+sudo cp $HOME/.local/src/its_osysinfo/its-osysinfo.service /etc/systemd/system/its-osysinfo.service
+sudo systemctl enable its-osysinfo
+sudo systemctl start its-osysinfo
