9c6096d5956f6bdcdadc812a1c9a7570ddd821ba
Joseph Turner <Tearran@users.noreply.github.com>
2022-10-08 20:02:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,13 @@ yes | rm -r .git || ( echo "faild rm .git" && exit )
 
 ## compress the contents to a zip
 sudo apt update
-sudo apt install zip python3-pip python3-pil libopenjp2-7
+sudo apt install git zip python3-pip python3-pil libopenjp2-7
 sudo pip3 install luma.core luma.lcd luma.oled psutil
-
 wget https://raw.githubusercontent.com/Tearran/its-i2cScan/main/build.bash && bash build.bash
 sleep 1 && clear
 its-its-i2cScan 1
+
+git clone https://github.com/Tearran/its_osysinfo.git ~/.local/src/its_osysinfo/; cd ~/.local/src/its_osysinfo/
 
 [ -e /usr/bin/zip ] && zip -r ../$execName.zip ./* 
 
